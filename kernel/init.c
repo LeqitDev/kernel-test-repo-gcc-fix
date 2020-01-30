ebd16dd9901b277542bda805221a128f8168388a
Markus Hamacher <markus.hamacher16@gmail.com>
2020-01-30 21:34:04 +0100

    init commit

---
+++ kernel/init.c
@@ -0,0 +1,19 @@
+void init(void)
+{
+    long long x = 1569325056;
+    long long y = 1569325106;
+    long long ergebnis = x % y;
+    const char hw[] = "Hello World!";
+    int i;
+    char* video = (char*) 0xb8000;
+
+    // C-Strings haben ein Nullbyte als Abschluss
+    for (i = 0; hw[i] != '\0'; i++) {
+
+        // Zeichen i in den Videospeicher kopieren
+        video[i * 2] = hw[i];
+
+        // 0x07 = Hellgrau auf Schwarz
+        video[i * 2 + 1] = 0x07;
+    }
+}
